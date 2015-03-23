a8d97143af6585bf8e98c970962bf9cf08cc27bc
damste <damicoste@gmail.com>
2015-03-23 21:35:59 +0100

    vjr BETWEEN() function framed

--- source/vjr/source/commands/command_defs.h
+++ source/vjr/source/commands/command_defs.h
@@ -180,6 +180,7 @@ struct SReturnsParams;
 	SVariable*			function_atc								(SThisCode* thisCode, SVariable* varNeedle, SVariable* varHaystack, SVariable* varOccurrence, SReturnsParams* returnsParams);
 	SVariable*			ifunction_at_occurs_common					(SThisCode* thisCode, SVariable* varNeedle, SVariable* varHaystack, SVariable* varOccurrence, bool tlCaseSensitive, bool tlScanBackward, u32* tnFoundCount, SReturnsParams* returnsParams);
 	SVariable*			function_atn2								(SThisCode* thisCode, SVariable* varY, SVariable* varX, SReturnsParams* returnsParams);
+	SVariable*			function_between	/* Stefano D'Amico */	(SThisCode* thisCode, SVariable* varValue, SVariable* varLowValue, SVariable* varHighValue, SReturnsParams* returnsParams);
 	SVariable*			function_bits								(SThisCode* thisCode, SVariable* varBits, SReturnsParams* returnsParams);
 	SVariable*			function_bits8								(SThisCode* thisCode, SVariable* varBits, SReturnsParams* returnsParams);
 	SVariable*			function_bits16								(SThisCode* thisCode, SVariable* varBits, SReturnsParams* returnsParams);
@@ -389,6 +390,7 @@ struct SReturnsParams;
 		{	_ICODE_ATAN,			1,			(uptr)&function_atan,			1,				1,				&gsSourceLight_atan[0]			},	// ATAN() by Stefano D'Amico, VJr 0.56, Mar.18.2015
 		{	_ICODE_ATC,				1,			(uptr)&function_atc,			2,				3,				&gsSourceLight_atc[0]			},
 		{	_ICODE_ATN2,			1,			(uptr)&function_atn2,			2,				2,				&gsSourceLight_atn2[0]			},
+		{	_ICODE_BETWEEN,			1,			(uptr)&function_between,		1,				1,				&gsSourceLight_between[0]		},	// BETWEEN() by Stefano D'Amico, VJr 0.57, Mar.23.2015
 		{	_ICODE_BITS,			1,			(uptr)&function_bits,			1,				1,				&gsSourceLight_bits[0]			},
 		{	_ICODE_BITS8,			1,			(uptr)&function_bits8,			1,				1,				&gsSourceLight_bits8[0]			},
 		{	_ICODE_BITS16,			1,			(uptr)&function_bits16,			1,				1,				&gsSourceLight_bits16[0]		},
